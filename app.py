eabd9a0b958269ec6a4336480c0f7595e60d20c0
Sabitha S <sabithas7049@gmail.com>
2026-03-14 17:03:03 +0530

    Change server port to 10000 for main execution

--- app.py
+++ app.py
@@ -377,3 +377,6 @@ if __name__ == '__main__':
     print("="*60 + "\n")
     
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
